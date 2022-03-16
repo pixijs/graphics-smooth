baebd0269b97015ffa823e58bc1c2c2157d680eb
dev7355608 <dev7355608@gmail.com>
2022-03-16 18:55:46 +0100

    Fix orientation (pixijs/pixijs#8223)

--- src/shapes/PolyBuilder.ts
+++ src/shapes/PolyBuilder.ts
@@ -7,6 +7,45 @@ import { earcut } from '@pixi/utils';
 
 const tempArr: Array<number> = [];
 
+function fixOrientation(points: number[], hole = false)
+{
+    const m = points.length;
+
+    if (m < 6)
+    {
+        return;
+    }
+
+    let area = 0;
+
+    for (let i = 0, x1 = points[m - 2], y1 = points[m - 1]; i < m; i += 2)
+    {
+        const x2 = points[i];
+        const y2 = points[i + 1];
+
+        area += (x2 - x1) * (y2 + y1);
+
+        x1 = x2;
+        y1 = y2;
+    }
+
+    if ((!hole && area > 0) || (hole && area <= 0))
+    {
+        const n = m / 2;
+
+        for (let i = n + (n % 2); i < m; i += 2)
+        {
+            const i1 = m - i - 2;
+            const i2 = m - i - 1;
+            const i3 = i;
+            const i4 = i + 1;
+
+            [points[i1], points[i3]] = [points[i3], points[i1]];
+            [points[i2], points[i4]] = [points[i4], points[i2]];
+        }
+    }
+}
+
 export class PolyBuilder implements IShapeBuilder
 {
     path(graphicsData: SmoothGraphicsData, buildData: BuildData)
@@ -303,11 +342,15 @@ export class PolyBuilder implements IShapeBuilder
         }
         const holeArray = [];
         let len = points.length;
-        // Process holes..
 
+        fixOrientation(points, false);
+
+        // Process holes..
         for (let i = 0; i < holes.length; i++)
         {
             const hole = holes[i];
+
+            fixOrientation(hole.points, true);
 
             holeArray.push(points.length / 2);
             points = points.concat(hole.points);
